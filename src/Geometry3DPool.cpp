f195d6ab312398f1667cf3526861945534609428
Alex Blinov <alexander@omi.so>
2024-01-03 10:00:06 +0000

    unsupported linux pragma fix

--- src/Geometry3DPool.cpp
+++ src/Geometry3DPool.cpp
@@ -188,7 +188,10 @@ struct PoolDetails_lt
                 {
                   // the normals are consistent so the quaternions should be too. If they aren't it means that there is a sign change
                   // in the rotation angle - we will introduce new triangles to avoid the sign change
+#ifndef WIN32
 #warning we modify verts so can't take references here
+#endif
+
                   const auto v1 = verts[n];
                   const auto v2 = verts[n+1];
                   const auto v3 = verts[n+2];
