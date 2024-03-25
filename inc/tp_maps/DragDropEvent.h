0ef082bf31d24f61fee0191462dd55a1a2068052
Tom Paynter <tompaynter@tdpe.co.uk>
2024-03-25 16:39:30 +0000

    Fix performance issues with the asset lists.

--- inc/tp_maps/DragDropEvent.h
+++ inc/tp_maps/DragDropEvent.h
@@ -10,11 +10,11 @@ namespace tp_maps
 //##################################################################################################
 enum class DragDropEventType
 {
-  None,
-  Drop,  //!< Event which is sent when a drag and drop action is completed
-  Enter, //!< Event which is sent to a widget when a drag and drop action enters it.
-  Move,  //!< Event which is sent while a drag and drop action is in progress.
-  Leave, //!< Event that is sent to a widget when a drag and drop action leaves it
+  None  = 0,
+  Drop  = 1, //!< Event which is sent when a drag and drop action is completed
+  Enter = 2, //!< Event which is sent to a widget when a drag and drop action enters it.
+  Move  = 3, //!< Event which is sent while a drag and drop action is in progress.
+  Leave = 4, //!< Event that is sent to a widget when a drag and drop action leaves it
 };
 
 //##################################################################################################
